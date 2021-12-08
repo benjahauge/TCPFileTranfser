7747deaa32da21da537d688bc75619e94527bc51
benjahauge <Benjahauge@hotmail.com>
2021-12-08 11:19:19 +0100

    Add project files.

---
+++ TCPFileTranfser/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Net.Sockets;
+using System.Text.Json;
+
+namespace TestFileTransfer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Wee");
+
+			string[] files = Directory.GetFiles(@"c:/PythonVideo/");
+			List<TransferClass> transferList = new List<TransferClass>();
+
+			foreach (var file in files)
+			{
+				byte[] bytes = File.ReadAllBytes(file);
+
+				TransferClass myTrans = new() { VideoName = file, Data = bytes };
+				transferList.Add(myTrans);
+			}
+
+
+			SendToRest(transferList);
+		}
+
+		public static void SendToRest(List<TransferClass> myTrans)
+		{
+			using (HttpClient client = new HttpClient())
+			{
+				JsonContent content = JsonContent.Create(myTrans);
+				HttpResponseMessage result = client.PostAsync("https://camsanctuary.azurewebsites.net/api/receiver/video", content).Result;
+				Console.WriteLine(result.StatusCode);
+			}
+		}
+	}
+
+	class TransferClass
+	{
+		public byte[] Data { get; set; }
+		public string VideoName { get; set; }
+	}
+}
